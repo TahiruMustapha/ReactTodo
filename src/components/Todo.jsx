96f304a5d46a5d871cd6105197a15f6dfbe99331
Mustapha <tahirumustapha13@gmail.com>
2024-08-09 18:04:42 +0200

    Made new changes

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,45 +1,51 @@
 import React, { useEffect, useState } from "react";
-
+import { v4 as uuidv4 } from "uuid";
 const Todo = () => {
+  let storedTask = JSON.parse(localStorage.getItem("task")) || [];
   const [newTask, setNewTask] = useState("");
   const [task, setTask] = useState([]);
   let [editTask, setEditTask] = useState("");
-  // const [editTaskIndex, setEditTaskIndex] = useState(null);
   const [editingId, setEditingId] = useState(null);
 
-  useEffect(() => {
-    if (localStorage.getItem("task")) {
-      const storedTask = JSON.parse(localStorage.getItem("task"));
-      setTask(storedTask);
-    }
-  }, []);
-
+  // useEffect(() => {
+  //   const storedTask = JSON.parse(localStorage.getItem("task"));
+  //   // setTask(storedTask);
+  // }, []);
+  
   const handleDelete = (index) => {
-    const newTask = task.filter((_, taskIndex) => taskIndex !== index);
-    setTask(newTask);
+    const tasks = JSON.parse(localStorage.getItem("task"));
+    const newTask = tasks.filter((_, taskIndex) => taskIndex !== index);
+    // setTask(newTask);
     localStorage.setItem("task", JSON.stringify(newTask));
   };
 
   const functionToMoveTaskUp = (index) => {
-    if (index > 0) {
-      const updatedTask = [...task];
-      [updatedTask[index], updatedTask[index - 1]] = [
-        updatedTask[index - 1],
-        updatedTask[index],
-      ];
-      setTask(updatedTask);
-      localStorage.setItem("task", JSON.stringify(updatedTask));
+    const tasks = JSON.parse(localStorage.getItem("task"));
+    if (tasks) {
+      const taskIndex = tasks.findIndex((task) => task.id === index);
+      if (taskIndex > 0) {
+        [tasks[taskIndex - 1], tasks[taskIndex]] = [
+          tasks[taskIndex],
+          tasks[taskIndex - 1],
+        ];
+        // setTask(tasks);
+        localStorage.setItem("task", JSON.stringify(tasks));
+      }
     }
   };
+
   const functionToMoveTaskDown = (index) => {
-    if (index < task.length - 1) {
-      const updatedTask = [...task];
-      [updatedTask[index], updatedTask[index + 1]] = [
-        updatedTask[index + 1],
-        updatedTask[index],
-      ];
-      setTask(updatedTask);
-      localStorage.setItem("task", JSON.stringify(updatedTask));
+    const tasks = JSON.parse(localStorage.getItem("task"));
+    if (tasks) {
+      const taskIndex = tasks.findIndex((task) => task.id === index);
+      if (taskIndex < tasks.length - 1) {
+        [tasks[taskIndex + 1], tasks[taskIndex]] = [
+          tasks[taskIndex],
+          tasks[taskIndex + 1],
+        ];
+        // setTask(tasks);
+        localStorage.setItem("task", JSON.stringify(tasks));
+      }
     }
   };
   const handleAddTask = (e) => {
@@ -48,37 +54,40 @@ const Todo = () => {
   const handleFormSUbmit = (e) => {
     e.preventDefault();
     if (newTask.trim() === "") return alert("Input a task!");
-    setTask((previousTask) => [...previousTask, newTask.trim()]);
-    localStorage.setItem("task", JSON.stringify([...task, newTask]));
+    const taskData = {
+      id: uuidv4(),
+      name: newTask,
+    };
+    // setTask((previousTask) => [...previousTask, taskData]);
+    localStorage.setItem("task", JSON.stringify([...storedTask, taskData]));
     setNewTask("");
   };
-  const showEditTaskInfo = (index, task) => {
-    setEditingId(index);
+  const showEditTaskInfo = (id, task) => {
+    setEditingId(id);
     setEditTask(task);
+    // console.log(id);
   };
 
   const handleEditTask = (e) => {
-    // setEditTaskInput(e.target.value);
     e.preventDefault();
     let value = e.target.value;
     setEditTask(value);
   };
   const updateTaskFunction = () => {
-    // e.preventDefault();
-    // console.log("clicked!")
     const edittedText = editTask.trim();
-    if (edittedText !== "" && editingId >= 0) {
+    if (edittedText !== "" && editingId) {
       editTaskFunction(editingId, edittedText);
-      // localStorage.setItem("task", JSON.stringify(edittedText));
-      // setEditTask("");
     } else {
       alert("Input something!");
     }
   };
   function editTaskFunction(index, newTodoTask) {
     const tasks = JSON.parse(localStorage.getItem("task"));
-    if (tasks && index < tasks.length) {
-      tasks[index] = newTodoTask;
+    if (tasks) {
+      const taskIndex = tasks.findIndex((task_s) => task_s.id === index);
+      if (taskIndex !== -1) {
+        tasks[taskIndex].name = newTodoTask;
+      }
       localStorage.setItem("task", JSON.stringify(tasks));
     }
   }
@@ -94,15 +103,19 @@ const Todo = () => {
         </form>
       </div>
       <div className="todoContainer">
-        {task.map((task, index) => (
+        {storedTask.map((task, index) => (
           <div key={index} className="taskBox">
-            <li onClick={() => showEditTaskInfo(index, task)} className="task">
-              {task}
+            <li
+              key={task.id}
+              onClick={() => showEditTaskInfo(task.id, task.name)}
+              className="task"
+            >
+              {task.name}
             </li>
             <p className="taskActions">
-              <span onClick={() => functionToMoveTaskUp(index)}>Up</span>
+              <span onClick={() => functionToMoveTaskUp(task.id)}>Up</span>
 
-              <span onClick={() => functionToMoveTaskDown(index)}>Down</span>
+              <span onClick={() => functionToMoveTaskDown(task.id)}>Down</span>
               <span onClick={() => handleDelete(index)}>Remove</span>
             </p>
           </div>
---
+++ src/components/dummy.js
@@ -0,0 +1,4 @@
+tasks= [
+    {id:1,task:"call mom"},
+    {id:2,task:"call dad"},
+]
